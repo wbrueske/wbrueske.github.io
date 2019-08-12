11f56915439212f146711c33d8e5e5b940ca8623
Will Brueske <wbrueske@live.com>
2019-08-12 11:04:13 -0500

    Updates

---
+++ precache-manifest.f96bb0171c043ff7834c5b5bb786daba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceaa8edcd413d539f54c7e282fed72a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "544fa2e5059cb0cc526e",
+    "url": "/static/css/main.e567d109.chunk.css"
+  },
+  {
+    "revision": "cf94486b7b4e6df8b276",
+    "url": "/static/js/2.12905dcf.chunk.js"
+  },
+  {
+    "revision": "544fa2e5059cb0cc526e",
+    "url": "/static/js/main.24555bda.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
